f0c2dba7433cc9c3db4700fcc597f8499e644bd9
Erwin Bovendeur <erwin@bovendeur.org>
2018-10-30 23:49:53 +0100

    Added default Hangfire.MvcApplication sample project

---
+++ samples/Hangfire.MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace Hangfire.MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View((object)TextBuffer.ToString());
+        }
+
+        public ActionResult Buffer()
+        {
+            return Content(TextBuffer.ToString());
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            BackgroundJob.Enqueue(() => TextBuffer.WriteLine("Background Job completed successfully!"));
+            TextBuffer.WriteLine("Background job has been created.");
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ samples/Hangfire.MvcApplication/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hangfire.MvcApplication.Startup))]
+
+namespace Hangfire.MvcApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("DefaultConnection");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+            
+            RecurringJob.AddOrUpdate(
+                () => TextBuffer.WriteLine("Recurring Job completed successfully!"), 
+                Cron.Minutely);
+        }
+    }
+}
